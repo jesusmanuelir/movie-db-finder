8eef7d544282c9ca6d84ea04429018ca42ca3f24
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-09-04 23:31:14 -0400

    add collapse

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -12,6 +12,7 @@ import NoFound from '../containers/NoFound';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/collapse';
 import '../assets/style/App.scss';
 
 const App = () => {
